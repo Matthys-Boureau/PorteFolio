531dbfd8578577714f9b105a1acefd670b51efc5
Matt <mboureau59@gmail.com>
2023-04-24 15:02:56 +0200

    maj animation lors du scroll pour l'article "work"

--- js/class.js
+++ js/class.js
@@ -1,23 +1,28 @@
-// Sélectionner les éléments HTML à modifier
+// Sélectionner les éléments HTML à modifier pour Menu
 const about = document.querySelector("#about");
 const work = document.querySelector("#work");
 const contact = document.querySelector("#contact");
+
+// Sélectionner les éléments HTML à modifier pour animation
+const workAnimation = document.querySelector("#work__Article");
 
 // Ajouter un événement de défilement à la fenêtre
 window.addEventListener("scroll", () => {
   // Vérifier la hauteur de la page au scroll et modifier les classes des éléments en conséquence
   const scrollTop = document.documentElement.scrollTop;
-  if (scrollTop <= 1100) {
+  if (scrollTop <= 1000) {
     about.classList.add("active");
     work.classList.remove("active");
     contact.classList.remove("active");
   } else if (scrollTop <= 2000) {
-    about.classList.remove("active");
     work.classList.add("active");
+    workAnimation.classList.add("animation");
+    about.classList.remove("active");
     contact.classList.remove("active");
   } else {
+    contact.classList.add("active");
     about.classList.remove("active");
     work.classList.remove("active");
-    contact.classList.add("active");
+    
   }
 });
