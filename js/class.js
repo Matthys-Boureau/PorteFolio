0442da8890ab25dda2b52d7ee8c3069cd09dc67c
Matt <mboureau59@gmail.com>
2023-05-03 15:23:22 +0200

    Responsive and scroll  animation ( screen < 1000 )

--- js/class.js
+++ js/class.js
@@ -8,44 +8,38 @@ const workAnimation = document.querySelector("#work__Article");
 const gridAnimation = document.querySelectorAll(".test");
 // Ajouter un événement de défilement à la fenêtre
 
-var aboutSize = 1000
-var workSize = 2000
-var gridAnim = 500
 
 
-if (window.screen.width <= 1000){
-  aboutSize += 1000;
-  workSize += 1000;
-  gridAnim += 1000;
-}
 
 
 window.addEventListener("scroll", () => {
   // Vérifier la hauteur de la page au scroll et modifier les classes des éléments en conséquence
   const scrollTop = document.documentElement.scrollTop;
-  console.log(window.screen.width);
+  var aboutSize = 1000
+  var workSize = 2000
+  var gridAnim = 500
 
-  if (window.screen.width <= 1000){
-    aboutSize += 1000;
-    workSize += 1000;
-    gridAnim += 1000;
-  }
+  if (window.screen.width < 1000){
+      aboutSize = 1700; 
+      workSize = 2700;
+    }
   
-  if (scrollTop <= aboutSize) {
+  if (scrollTop < aboutSize) {
     about.classList.add("active");
     work.classList.remove("active");
     contact.classList.remove("active");
 
-  } else if (scrollTop <= workSize) {
+  } else if (scrollTop < workSize) {
     work.classList.add("active");
     about.classList.remove("active");
     contact.classList.remove("active");
 
-  }else {
+  }else{
     contact.classList.add("active");
     about.classList.remove("active");
     work.classList.remove("active");
   }
+
   if (scrollTop >= gridAnim) {
       workAnimation.classList.add("animation");
       gridAnimation.forEach(element => {
